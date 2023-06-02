6fe6f8077806fcb61bc05a1fabbfcd37858262b0
Leticia12 <leticia.silva@arphoenix.com.br>
2023-06-02 16:36:00 -0300

    Fazendo alguns testes no proejto

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,11 +1,5 @@
 
 \
 ,src/main/java/org/example/model/Contato.java,4\2\4290ac073441aa416c886125a01ed9491a376aa7
-`
-0src/test/java/org/example/model/ContatoTest.java,8\2\823d777955233c0b9bc17e4e5730e5c85bbadc7e
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
-S
-#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
-k
-;src/main/java/org/example/repository/ContatoRepository.java,d\4\d49f2b033548aa1aacf0fb48ccf2a3a8ee2be795
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,33 @@
 package org.example;
 
+import org.example.model.Contato;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
+
+        ArrayList<Contato> contatos = new ArrayList<>();
+
+        Contato contato1 = new Contato(1L, "Leticia", 99999999);
+        Contato contato2 = new Contato(2L, "Rafael", 888888888);
+        Contato contato3 = new Contato(3L, "Larissa", 777777777);
+        Contato contato4 = new Contato(4L, "Fernando", 666666666);
+        Contato contato5 = new Contato(5L, "Aparecida", 444444444);
+
+        contatos.add(contato1);
+        contatos.add(contato2);
+        contatos.add(contato3);
+        contatos.add(contato4);
+        contatos.add(contato5);
+
+        for (Contato c : contatos) {
+            System.out.println(c.getNome());
+        }
 
 
+        
 
     }
 }
